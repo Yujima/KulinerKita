79c849f3e37071d2fce675441da64d563b41b5ba
Yujima <113968464+Yujima@users.noreply.github.com>
2023-04-17 07:58:45 +0700

    Update (17.04.2023)

---
+++ app/src/main/java/com/if4a/kulinerkita/Activity/TambahActivity.java
@@ -0,0 +1,16 @@
+package com.if4a.kulinerkita.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.if4a.kulinerkita.R;
+
+public class TambahActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah);
+    }
+}
