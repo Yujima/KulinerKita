68d175d6ceb1f3644ba16d72f204639c40fe41dc
Yujima <wahyuaji.nurrahman@gmail.com>
2023-05-22 08:52:45 +0700

    Update File (22.05.2023)

--- app/src/main/java/com/if4a/kulinerkita/Activity/MainActivity.java
+++ app/src/main/java/com/if4a/kulinerkita/Activity/MainActivity.java
@@ -4,14 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.if4a.kulinerkita.API.APIRequestData;
+import com.if4a.kulinerkita.API.RetroServer;
+import com.if4a.kulinerkita.Adapter.AdapterKuliner;
 import com.if4a.kulinerkita.Model.ModelKuliner;
+import com.if4a.kulinerkita.Model.ModelResponse;
 import com.if4a.kulinerkita.R;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView rvKuliner;
@@ -29,5 +40,42 @@ public class MainActivity extends AppCompatActivity {
         fabTambah = findViewById(R.id.fab_tambah);
 
         lmKuliner = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        rvKuliner.setLayoutManager(lmKuliner);
+
+        fabTambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TambahActivity.class));
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        retriveKuliner();
+    }
+
+    private void retriveKuliner() {
+        APIRequestData ARD = RetroServer.konekRetrofit().create(APIRequestData.class);
+        Call<ModelResponse> proses = ARD.ardRetrive();
+
+        proses.enqueue(new Callback<ModelResponse>() {
+            @Override
+            public void onResponse(Call<ModelResponse> call, Response<ModelResponse> response) {
+                String kode = response.body().getKode();
+                String pesan = response.body().getPesan();
+                listKuliner = response.body().getData();
+
+                adKuliner = new AdapterKuliner(MainActivity.this, listKuliner);
+                rvKuliner.setAdapter(adKuliner);
+                adKuliner.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<ModelResponse> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Gagal Menghubungi Server", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/if4a/kulinerkita/Activity/TambahActivity.java
+++ app/src/main/java/com/if4a/kulinerkita/Activity/TambahActivity.java
@@ -3,14 +3,77 @@ package com.if4a.kulinerkita.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.if4a.kulinerkita.API.APIRequestData;
+import com.if4a.kulinerkita.API.RetroServer;
+import com.if4a.kulinerkita.Model.ModelResponse;
 import com.if4a.kulinerkita.R;
 
-public class TambahActivity extends AppCompatActivity {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
+public class TambahActivity extends AppCompatActivity {
+    private EditText etNama, etAsal, etDeskripsiSingkat;
+    private Button btnSimpan;
+    private String nama, asal, deskripsiSingkat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah);
+
+        etNama = findViewById(R.id.et_namaKuliner);
+        etAsal = findViewById(R.id.et_asalKuliner);
+        etDeskripsiSingkat = findViewById(R.id.et_deskripsiSingkat);
+        btnSimpan = findViewById(R.id.btn_tambah);
+
+        btnSimpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nama = etNama.getText().toString();
+                asal = etAsal.getText().toString();
+                deskripsiSingkat = etDeskripsiSingkat.getText().toString();
+
+                if(nama.trim().isEmpty()||asal.trim().isEmpty()||deskripsiSingkat.trim().isEmpty()){
+                    if(nama.trim().isEmpty()){
+                        etNama.setError("Nama Tidak Boleh Kosong");
+                    }
+                    if(asal.trim().isEmpty()){
+                        etAsal.setError("Asal Tidak Boleh Kosong");
+                    }
+                    if(deskripsiSingkat.trim().isEmpty()){
+                        etDeskripsiSingkat.setError("Deskripsi Tidak Boleh Kosong");
+                    }
+                }
+                else{
+                    tambahKuliner();
+                }
+            }
+        });
+    }
+
+    private void tambahKuliner(){
+        APIRequestData ARD = RetroServer.konekRetrofit().create(APIRequestData.class);
+        Call<ModelResponse> proses = ARD.ardCreate(nama, asal, deskripsiSingkat);
+
+        proses.enqueue(new Callback<ModelResponse>() {
+            @Override
+            public void onResponse(Call<ModelResponse> call, Response<ModelResponse> response) {
+                String kode = response.body().getKode();
+                String pesan = response.body().getPesan();
+
+                Toast.makeText(TambahActivity.this, "Kode : " + kode + "Pesan : " + pesan, Toast.LENGTH_SHORT).show();
+                finish();
+            }
+
+            @Override
+            public void onFailure(Call<ModelResponse> call, Throwable t) {
+                Toast.makeText(TambahActivity.this, "Gagal Menghubungi Server", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
